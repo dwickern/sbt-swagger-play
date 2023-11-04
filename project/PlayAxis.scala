1cb9a63ba82cf1ebd0f70a9f023a3a32670d96cb
Derek Wickern <dwickern@gmail.com>
2023-11-04 12:11:57 -0700

    refactor PlayAxis

--- project/PlayAxis.scala
+++ project/PlayAxis.scala
@@ -2,12 +2,12 @@ import sbt.ScriptedPlugin.autoImport._
 import sbt._
 import sbt.internal.ProjectMatrix
 
-case class PlayAxis(version: String, idSuffix: String, directorySuffix: String) extends VirtualAxis.WeakAxis
+case class PlayAxis(version: String) extends VirtualAxis.WeakAxis {
+  val directorySuffix: String = "-play" + version
+  val idSuffix: String = directorySuffix.replace('.', '_')
+}
 
 object PlayAxis {
-  def apply(version: String): PlayAxis =
-    PlayAxis(version, "-play" + version.replace('.', '_'), "-play" + version)
-
   implicit class RichProjectMatrix(val matrix: ProjectMatrix) extends AnyVal {
     def scriptedTests(playAxis: PlayAxis, scalaVersion: String): ProjectMatrix = matrix.customRow(
       autoScalaLibrary = false,
